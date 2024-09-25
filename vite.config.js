9e81f107b4d62c3710e6c6524a3ee14a0ef003f7
Faiz Mohamad <153068612+mfaizmohamad@users.noreply.github.com>
2024-09-25 14:39:58 +0800

    deployment fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/portfolio_2024/",
+  base: '/portfolio_2024/',  // Set your base path for GitHub Pages deployment
   plugins: [react()],
-  assetsInclude: ['**/*.glb'],
-})
+  assetsInclude: ['**/*.glb'],  // Include GLB assets in the build
+  build: {
+    chunkSizeWarningLimit: 2000,  // Increase this to suppress warnings for large chunks
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Split your large files or libraries into separate chunks
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+          if (id.includes('.glb')) {
+            return 'models';
+          }
+        },
+      },
+    },
+  },
+});
